771fcf403ed1721aced7d2ef5a04b192db61da06
Nico Wicke <gameplay50@gmail.com>
2024-12-19 21:36:58 +0100

    A001 Added Structure, GameControl, Version

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl control;
+
+    /// <summary>
+    /// State of development. (ALPHA, BETA, ...)
+    /// </summary>
+    public Enums.E_Version developmentState;
+    /// <summary>
+    /// Version of the profile that is saved with A, B, ...
+    /// </summary>
+    public string profileVersion;
+    /// <summary>
+    /// Game version of the build 001, 002, ...
+    /// </summary>
+    public string gameVersion;
+
+    void Awake()
+    {
+        // consistant game controller with data
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+}
