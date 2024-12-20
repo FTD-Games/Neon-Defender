221364497e19aa95c9099b11fea8e066222e0013
VeroxCode <kevcrafterhd.kc@gmail.com>
2024-12-20 18:04:49 +0100

    Added BaseStats Class, Implemented BaseStats into Player

---
+++ Assets/Scripts/Functions/BaseStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+
+    public float Health;
+    public float Armor;
+    public float Speed;
+    public float Damage;
+    public float Experience;
+    public float CritChance;
+    public float CritDamage;
+
+}
